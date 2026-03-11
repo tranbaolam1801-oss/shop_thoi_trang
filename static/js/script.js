67d0204639d93f94c9bbefc436ab469aee22a139
Tran Bao Lam <tranbaolam1801@gmail.com>
2026-03-11 21:42:31 +0700

    upload django project

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+console.log("Shop thời trang đã load!");
+
+function addToCart(name){
+    alert(name + " đã được thêm vào giỏ hàng!");
+}
+
+function showWelcome(){
+    alert("Chào mừng bạn đến với Shop Thời Trang!");
+}
+
+window.onload = function(){
+    console.log("Website ready!");
+}
